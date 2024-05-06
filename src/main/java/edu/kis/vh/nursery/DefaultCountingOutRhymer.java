60896cfa5dfccfa216fbd9171f89613f72477b9e
JakubMagdziak <114910578+JakubMagdziak@users.noreply.github.com>
2024-05-06 15:14:22 +0200

    zad 6

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int SIZE = 12;
+    public static final int TOT = -1;
+    private int[] numbers = new int[SIZE];
 
-    public int total = -1;
+    public int total = TOT;
 
     public void countIn(int in) {
         if (!isFull())
