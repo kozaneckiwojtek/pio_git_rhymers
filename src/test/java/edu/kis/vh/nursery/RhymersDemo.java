0638959ff82173a1c10e7a6b9b4302c8c2111441
Wojciech Kozanecki <245848@edu.p.lodz.pl>
2024-05-27 14:52:34 +0200

    13. dodano metodę statyczną testRhymers

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -8,6 +8,11 @@ class RhymersDemo {
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
 
+        testRhymers(factory);
+
+    }
+
+    private static void testRhymers(RhymersFactory factory) {
         final int NUM_OF_RHYMERS = 3;
         final int NUM_OF_ELEMENTS = 15;
         final int BOUND = 20;
@@ -15,24 +20,23 @@ class RhymersDemo {
 
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
+
         for (int i = 1; i < NUM_OF_ELEMENTS; i++)
             for (int j = 0; j < NUM_OF_RHYMERS; j++)
                 rhymers[j].countIn(i);
-        
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < NUM_OF_ELEMENTS; i++)
             rhymers[HANOI_RHYMER].countIn(rn.nextInt(BOUND));
-        
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[HANOI_RHYMER]).reportRejected());
-        
     }
-    
+
 }
